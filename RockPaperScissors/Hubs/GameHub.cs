fe744e32698fd2cdb8e1d9062e6bf649c28bbad4
DanailY <Danail.V.Yordanov@gmail.com>
2020-04-19 13:34:06 +0300

    Introducing SignalR

---
+++ RockPaperScissors/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendChosenShape(string user, string shape)
+        {
+            await Clients.All.SendAsync("ReceiveShape", user, shape);
+        }
+    }
+}
--- RockPaperScissors/Startup.cs
+++ RockPaperScissors/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RockPaperScissors.Hubs;
 
 namespace RockPaperScissors
 {
@@ -24,6 +25,7 @@ namespace RockPaperScissors
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +52,7 @@ namespace RockPaperScissors
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<GameHub>("/gameHub");
             });
         }
     }
